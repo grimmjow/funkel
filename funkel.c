4071231955692be83b77091a321f60909fab81df
grimm <chis.o.henning@gmail.com>
2012-06-05 23:20:37 +0200

    func_x with dummy timer

--- funkel.c
+++ funkel.c
@@ -9,6 +9,8 @@ unsigned char led[8];
 unsigned long int rotation_time = 1;
 unsigned long int time_count = 1;
 
+unsigned long test = 0;
+
 
 void init_timer0() {
     TCCR0 = 0x00;       // Stop Timer/Counter0
@@ -21,7 +23,7 @@ void init_interupt() {
 	// disable alternate functions
 	TCCR1A &= ~((1 << COM1B0) | (1 << COM1B1));
 
-	GIMSK |=  (1<<PCIE0);
+	GIMSK |= (1<<PCIE0);
 	GIMSK |= (1<<INT0);      // turn on interrupts!
 	MCUCR |= (1<<ISC00) | (1<<ISC01);
 
@@ -40,16 +42,40 @@ ISR(TIMER0_OVF0_vect) {
  * rising edge trigger
  */
 ISR (IO_PINS_vect) {
-	rotation_time = time_count;
+	//rotation_time = time_count;
 
-	time_count = 1;
+	//time_count = 1;
 }
 
 void set_leds() {
 
+	PORTA = (led[6] << PA2)
+			| (led[7] << PA3)
+			| (led[0] << PA4)
+	        | (led[1] << PA5)
+	        | (led[2] << PA6)
+	        | (led[3] << PA7);
+
+	PORTB = (1 << PB3)
+	        | (led[4] << PB4)
+	        | (led[5] << PB5);
+
 }
 
 void refresh (unsigned char current_x) {
+
+	unsigned long the_one_on = current_x / 8;
+
+	for (int i = 0; i < 8; i++) {
+		if (the_one_on == i + 1) {
+			led[i] = 1;
+		}
+		else {
+			led[i] = 0;
+		}
+	}
+
+	set_leds();
 
 }
 
@@ -57,25 +83,34 @@ int main() {
     init_interupt();
     init_timer0();
 
-    DDRA  = _BV(PA0);
-    PORTA |= _BV(PA0);
-    PORTB = 0x00;
-    DDRB  = 0;
+    DDRA  = 0xF0 | (1 << PA2) | (1 << PA3);
+    PORTA = 0xFF;
+    DDRB  = 0xF0;
+    PORTB = 0xFF;
 
     sei();
 
-    unsigned char last_x = X_RES + 1;
-    unsigned char current_x;
+    unsigned long last_x = X_RES + 1;
+    unsigned long current_x;
 
     while (1) {
-    	current_x = X_RES / (rotation_time / time_count);
+    	current_x = X_RES * time_count / rotation_time;
 
     	if (current_x == last_x) {
     		asm("sleep"::);
     	}
     	else {
-    		refresh(current_x);
+			refresh(current_x);
     	}
+
+    	test++;
+
+    	if (test > 800) {
+    		rotation_time = time_count;
+			time_count = 1;
+			test = 0;
+    	}
+
     }
 
     return 0;
