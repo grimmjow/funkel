39b76d84af1cc91c063ff247aa2810ee8dad6eac
grimm <chis.o.henning@gmail.com>
2012-10-24 23:43:55 +0200

    #1 generiere Bildarray noch ohne output

--- src/python/convert.py
+++ src/python/convert.py
@@ -26,12 +26,13 @@ for arg in sys.argv:
     elif (arg[:3] == "-t:") & (arg[3:].isdigit()):
         resolution["t"] = int(arg[3:])
 
-    else:
+    elif (arg == "-h") | (arg == "--help"):
         printHelp()
 
 img = Image.open(imageFile)
 pix = img.load()
 size = img.size
+radius = min(size[0] / 2, size[1] / 2)
 
 output = []
 
@@ -40,12 +41,27 @@ for z in range(resolution["z"]):
     last = []
     output.append(last)
     for x in range(resolution["x"]):
-        last.append((0, 0))
+        last.append([0, 0])
 
 # Pixel des Bildes durchlaufen
-for x in range(size[0]):
-    for y in range(size[1]):
-        pass
+for imgx in range(1, size[0]):
+    for imgy in range(1, size[1]):
+        a = imgx - (size[0] / 2)
+        b = imgy - (size[1] / 2)
+        c = math.sqrt(math.pow(imgx, 2) * math.pow(imgy, 2))
 
+        if c > radius:
+            continue
 
+        z = int(round((360 + math.degrees(math.sin(a/c))) % 360 / resolution["z"]))
+        x = int(round(c / resolution["x"]))
+        output[z][x][0] += pix[imgx, imgy]
+        output[z][x][1] += 1
 
+# export array
+for z in range(resolution["z"]):
+    for x in range(resolution["x"]):
+        if output[z][x][1] > 0:
+            output[z][x][0] = output[z][x][0] / output[z][x][1]
+        output[z][x][0] = int(round(output[z][x][0] / 128))
+        print output[z][x][0]
