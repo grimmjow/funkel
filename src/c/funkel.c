2737158e62fcfac52a8e5ea6fb24190f91dcd043
Christian Anke <carcinoma@gmx.net>
2013-10-08 21:24:37 +0200

    does not work

--- src/c/funkel.c
+++ src/c/funkel.c
@@ -6,9 +6,11 @@
 #include "imgdata.h"
 
 uint8_t volatile led[RES_Y/8];
-uint32_t volatile rotation_time = 1;
-
+uint32_t volatile rotation_time = 0x0000FFFF;
+uint8_t volatile myswitch = 0;
+uint32_t volatile time_index;
 uint32_t res_t = RES_T;
+uint8_t debug_led=1;
 
 #define BANK_0 PB4
 #define BANK_1 PB5
@@ -18,17 +20,17 @@ uint32_t res_t = RES_T;
 void init_timer0() {
 
     TCCR0B = 0x00;      // Stop Timer/Counter0
-    TCNT0L = 0;         // initial value
     TCNT0H = 0;
+    TCNT0L = 0;         // initial value
     TCCR0A = _BV(TCW0);  // 16-bit Mode, Incrementing to 0xFFFF
-    TCCR0B = (1 << CS02); // 8.000.000/256
-    //TCCR0B = (1 << CS02) | (1 << CS00); // 8.000.000/1024
+    //TCCR0B = (1 << CS02); // 8.000.000/256
+    TCCR0B = (1 << CS02) | (1 << CS00); // 8.000.000/1024
 
 }
 
 void init_interupt() {
 
-    MCUCR |= (1<<ISC00) | (1<<ISC01); // rising edge
+    MCUCR |= _BV(ISC00) | _BV(ISC01); // rising edge
     GIMSK |= _BV(INT0);
 
 }
@@ -42,19 +44,6 @@ uint32_t get_time() {
     value |= TCNT0L;
 
     return value;
-
-}
-
-/*
- * rising edge trigger
- */
-ISR (INT0_vect) {
-
-    if (get_time() > (rotation_time >> 1)) {
-        rotation_time = get_time();
-        TCNT0H = 0;
-        TCNT0L = 0;
-    }
 
 }
 
@@ -85,13 +74,18 @@ void set_leds() {
 
 }
 
-void display_image_data (uint32_t time_index) {
+void display_image_data () {
+    
+    led[0] = myswitch;
+    led[1] = (uint8_t)time_index;
+    led[2] = ~myswitch;
+    led[3] = ~myswitch;
 
     // read data from program memory into led arrays
-    led[0] = pgm_read_byte(&(imgdata[time_index][3]));
-    led[1] = pgm_read_byte(&(imgdata[time_index][2]));
-    led[2] = pgm_read_byte(&(imgdata[time_index][1]));
-    led[3] = pgm_read_byte(&(imgdata[time_index][0]));
+    //led[0] = imgdata[time_index][3];
+    //led[1] = imgdata[time_index][2];
+    //led[2] = led[0];
+    //led[3] = led[0];
 
     set_leds();
 
@@ -113,6 +107,11 @@ void display_standby (uint8_t val) {
 void display_lauflicht (uint32_t time_index) {
     
     time_index /= (res_t / 32);
+	
+    led[0] = 0;
+    led[1] = 0;
+    led[2] = 0;
+    led[3] = 0;
     
     if(time_index < 8) {
         led[0] = (1 << time_index);
@@ -131,14 +130,28 @@ void display_lauflicht (uint32_t time_index) {
 
 void display_time_index (uint32_t time_index) {
     
-    uint32_t val = time_index;
+    uint8_t val = 0xFF & time_index;
 
-    led[3] = 0;
-    led[2] = val >> 16;
-    led[1] = val >> 8;
+    led[3] = myswitch;
+    led[2] = 0;
+    led[1] = 0;
     led[0] = val;
     
     set_leds();
+
+}
+
+/*
+ * rising edge trigger
+ */
+ISR (INT0_vect) {
+
+    if (TCNT0H > 0x01) {
+        rotation_time = get_time();
+        myswitch = ~myswitch;
+        TCNT0H = 0;
+        TCNT0L = 0;
+    }
 
 }
 
@@ -157,32 +170,35 @@ int main() {
     led[2] = 0;
     led[3] = 0;
     set_leds();
-    
-    sei();
 
     uint32_t last_t = res_t + 1;
-    uint32_t time_index;
+    uint32_t time;
+
+    sei();
 
     while (1) {
-
+    
+        /*cli();
+        display_time_index(get_time());
+        sei();*/
         //    time_indeximer_ticks      x                time_indeximer_ticks * RES_T
         //   --------------------- = -----    =>    x = ---------------------------
         //   average_rotation_time   RES_T                 average_rotation_time
-        time_index = res_t * get_time() / rotation_time;
-        
-        if (time_index != last_t) {
-            last_t = time_index;
-            
-            if (get_time() > rotation_time) {
-                display_standby(0xF0);
-            }
-            else if(time_index >= res_t) {
-                display_standby(0b11001100);
-            } else {
-                display_image_data(time_index);
-            }
-        }
 
+        time = get_time();
+        time_index = res_t * time / rotation_time;
+
+        if (time_index != last_t) {
+            
+            last_t = time_index;
+
+            if (time > rotation_time) {
+                display_standby(0xF0);
+            } else {
+                display_time_index(time_index);
+            }
+            
+        }
     }
 
     return 0;
